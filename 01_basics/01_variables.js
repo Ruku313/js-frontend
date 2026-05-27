2360782921b37cfa743be7ef6176b7674826e2e5
Ruku313 <raktimdas313@gmail.com>
2026-05-27 19:02:09 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553; // can not be changed
+let accountEmail = "Raktim313@gmail.com";
+var accpassword = "12345";
+accountCity = "Howrah";
+let accountState;
+
+
+// console.table([accountId,accountEmail,accpassword,accountCity]);
+
+
+accountEmail = "Rd@rd.com";
+accpassword = "45421302";
+accountCity = "Tribeni";
+
+/*
+prefer not to use var because the issue in block scope 
+functional scope
+*/
+
+console.table([accountId,accountEmail,accpassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello"); we are using node js not browser
+
+console.log(3+3); // code readablity should be high
+
+//tc39.es  documentation
+
+let name = "Raktim";
+let age = 18;
+let isloggedin = true ;
+
+
+// datatypes 
+/*
+    number => 2^53
+    String => ""
+    boolean => true , false
+    null => stand alone value , important 
+    undefined ==> value not defined 
+    null representation of empty value , you can define value as none
+    symbol => mostly used for uniqueness
+    object
+*/
+
+console.log(typeof "Raktim");
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
