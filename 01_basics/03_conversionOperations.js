c213f96795aba65ae883f79f56880b8cfd70044e
Ruku313 <raktimdas313@gmail.com>
2026-05-27 19:34:22 +0000

    conversion Operators

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -12,17 +12,29 @@
 // "33abc" => NaN
 // true => 1 ; false => 0
 
-let isloggedin = 1;
-let booleanIsLoggedIn = Boolean(isloggedin);
-console.log(typeof isloggedin);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isloggedin = 1;
+// let booleanIsLoggedIn = Boolean(isloggedin);
+// console.log(typeof isloggedin);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> true ; 0 => false
 // "" => false;
 // "Raktim" = true;
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********** Operations ***********
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// let str1 = "Raktim";
+// let str2 = " Das";
+// let str3 = str1 + str2;
+// console.log(str3);
+// console.log(+true);
