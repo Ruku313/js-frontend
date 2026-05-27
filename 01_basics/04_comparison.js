2309c17c4bedebdc8a379d3427d7824bd5af1617
Ruku313 <raktimdas313@gmail.com>
2026-05-27 19:47:13 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// same type conversion => no issue 
+//  but differsent type comparison sometimes gives unpredictable issues
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
