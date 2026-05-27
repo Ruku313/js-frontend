0819cf9064edb700f33f46f475a68956c2e87b6f
Ruku313 <raktimdas313@gmail.com>
2026-05-27 19:21:29 +0000

    conversionOf Datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+// let score = "333Raktim"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+// console.log(typeof (Nan));
+
+// "33" =>  33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isloggedin = 1;
+let booleanIsLoggedIn = Boolean(isloggedin);
+console.log(typeof isloggedin);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0 => false
+// "" => false;
+// "Raktim" = true;
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
--- 01_basics/test2.js
+++
@@ -1,1 +0,0 @@
-console.log("Raktim Das");
