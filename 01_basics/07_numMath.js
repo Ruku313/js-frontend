0741928d0c42cbf1fc3656d94c4d9c9275cc048f
Ruku313 <raktimdas313@gmail.com>
2026-05-28 06:57:27 +0000

    math and num

---
+++ 01_basics/07_numMath.js
@@ -0,0 +1,34 @@
+// const score = 400;
+
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(score );
+
+// console.log(balance.toString());
+// console.log(typeof balance);
+
+// const otherNumber = 129.266;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// *********** MATHS ************
+
+console.log(Math.random());
+/**
+ * Math.abs()
+ * Math.round()
+ * Math.ceil()
+ * Math.floor()
+ * Math.sqrt()
+ * Math.min()
+ * Math.max()
+ * Math.random() --> very important
+ */
+
+// console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
